5969ceea96113d45aa3bfa374577057f87cdc9d1
chazzels <chazzels22@gmail.com>
2021-04-14 19:53:44 +0000

    saving work on the bot

--- src/culturaenturopa/culturaController.js
+++ src/culturaenturopa/culturaController.js
@@ -5,8 +5,6 @@ const shasum = crypto.createHash('sha256')
 const PuppetController = require('../core/puppetController');
 
 console.log(crypto.createHash("sha256").update("foo").digest().toString('hex'));
-console.log(crypto.createHash("sha256").update("bar").digest().toString('hex'));
-console.log(crypto.createHash("sha256").update("foo").digest().toString('hex'));
 
 class CulturaController extends PuppetController {
 	
@@ -16,21 +14,48 @@ class CulturaController extends PuppetController {
 		
 		this.inventory = new Map();
 		this.inventory.set('29472942378', {qty: 0, name: 'necklace'});
-		this.log(this.inventory);
+		this.log("map test: ",this.inventory);
 		
 		(async () => {
 			await this.initialConnection();
+			await this.scanInventory();
 		})();
 		
 	}
 	
-	async sampleInventory() {
-
-		// item container: product-list-wrapper[data-hook]
-		// inventory class: product-list-grid-item [data-hook]
+	async scanInventory() {
+		
+		this.log('InventoryScan', 'Running...');
+		
+		// inventory class: li product-list-grid-item [data-hook]
+		// inventory name: h3 product-item-name [data-hook]
+		// out of stock: span product-item-out-of-stock [data-hook]
+		
+		let items = await this.page.evaluate(
+			() => Array.from(document.querySelectorAll('li[data-hook="product-list-grid-item"]'), 
+			element => element.textContent)
+		);
+		this.log('items:', items.length);
+		
+		let names = await this.page.evaluate(
+			() => Array.from(document.querySelectorAll('h3[data-hook="product-item-name"]'), 
+			element => element.textContent)
+		);
+		this.log('names:', names.length);
+		
+		let oos = await this.page.evaluate(
+			() => Array.from(document.querySelectorAll('span[data-hook="product-item-out-of-stock"]'), 
+			element => element.textContent)
+		);
+		this.log('outOfStock:', oos.length);
+		
+		this.log('InventoryScan', 'Completed');
 		
 	}
 	
 }
+CulturaController.itemContainer = 'li[data-hook="product-list-grid-item"]';
+CulturaController.itemName = 'h3[data-hook="product-item-name"]';
+CulturaController.itemOutOfStock = 'span[data-hook="product-item-out-of-stock"]';
 
 var watcher = new CulturaController('CulturaWatcher', 'https://www.culturaenturopa.store/shop?page=10');
